65019ff0a248e22e6003a00b40e01a0404accf88
NicolasHHH <1538109712@qq.com>
2021-08-25 23:14:57 +0200

    Binary java

---
+++ Java101/src/INF4112021/Binary.java
@@ -0,0 +1,15 @@
+package INF4112021;
+
+public class Binary {
+
+
+    public static void main(String[] args){
+        int k = 15;
+        System.out.println(k<<1) ; // 1248=15 24816=30
+        k= 3;
+        System.out.println(k>>>1);
+        k= -3;
+        System.out.println(k>>1);
+
+    }
+}
